43a1665b3777dd33c2931389e30ea72a900631b3
Jason Yee <jason.yee@datadoghq.com>
2019-12-01 10:27:18 -0800

    add logging

--- backend/handler.py
+++ backend/handler.py
@@ -3,27 +3,30 @@ from datadog_lambda.wrapper import datadog_lambda_wrapper
 import json
 import requests
 
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 @datadog_lambda_wrapper
 def hotdogsave(event, context):
     body = json.loads(event["body"])
     dog = body["dog"]
     filename = dog[dog.rfind("/")+1:]
 
-    r = requests.get(dog, allow_redirects = True)
+    if "terrier" in dog:
+        code = 500
+        message = "WOOF! No terriers allowed!"
+        logger.info(message)
+        raise Exception(message)
+    else:
+        code = 200
+        message = "Here's where we should save {} to an S3 bucket".format(filename)
+        logger.info(message)
 
-    s3 = boto3.client('s3', region_name = 'us-east-1')
-    #response = s3.upload_fileobj(r.content, "hot-dog-saves", filename)
-    response = s3.put_object(
-        Bucket='hot-dog-saves',
-        Body=r.content,
-        Key=filename
-    )
-
-    print(json.dumps(response))
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(response)
+    return {
+        "statusCode": code,
+        "body": {
+            "message": message,
+            "filename": filename
+        }
     }
-
-    return response
